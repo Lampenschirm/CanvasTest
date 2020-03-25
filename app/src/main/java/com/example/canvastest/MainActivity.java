26d2d68d59995d708edd39d8ca3dc23169727f5e
adfral8 <alifrogfried@gmx.de>
2020-03-25 14:30:16 +0100

    extends with class to use GestureDetector

--- app/src/main/java/com/example/canvastest/MainActivity.java
+++ app/src/main/java/com/example/canvastest/MainActivity.java
@@ -3,15 +3,16 @@ package com.example.canvastest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
 
 public class MainActivity extends AppCompatActivity {
-
+ protected GestureDetector joystickGestureDetectorListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         viewCustomObject circle =  findViewById(R.id.Circle);
-
+        joystickGestureDetectorListener = new GestureDetector(this, new JoystickGestureDetectorListener());
     }
 }
